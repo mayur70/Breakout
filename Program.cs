ad3e77ccc53fab2757e4410b2b148fe55651b13d
Mayur Patil <mayurpatil001@hotmail.com>
2019-11-21 21:47:33 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Breakout {
+    public static class Program {
+        [STAThread]
+        static void Main () {
+            using (Main game = new Main ())
+            game.Run ();
+        }
+    }
+}
